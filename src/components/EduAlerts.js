0508a26db76a0f87d40bfc87094bab1d07488207
memery1446 <memery1446@gmail.com>
2025-04-16 09:09:56 -0400

    preDocker

--- src/components/EduAlerts.js
+++ src/components/EduAlerts.js
@@ -4,9 +4,9 @@ const EduAlerts = {
   HIGH_RISK_OLD_DATE: "WARNING: This approval is over a year old.",
   HIGH_RISK_DAI: "This approval is over a year old.",
   MEDIUM_RISK_LOW_AMOUNT: "Approval amount is below 500 tokens.",
-  MEDIUM_RISK_SINGLE_NFT: "Approval is for a single NFT, not full collection.",
-  DEFAULT_HIGH: "This approval is unlimited. Revoke today.",
-  DEFAULT_MEDIUM: "This approval is unlimited. Revoke today.",
+  MEDIUM_RISK_SINGLE_NFT: "This spender has access to one of your NFTs.",
+  DEFAULT_HIGH: "This spender has unlimited access to your assets.",
+  DEFAULT_MEDIUM: "This spender has the same access to your assets as you do. Revoke it today.",
 };
 
 export default EduAlerts;
